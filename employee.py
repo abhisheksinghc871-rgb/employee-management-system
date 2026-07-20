35576c22cad379c7a385bf731918062d9839b348
Abhishek Singh Chauhan <abhisheksinghc871@gmail.com>
2026-07-20 17:24:22 +0000

    Added employee search feature

--- employee.py
+++ employee.py
@@ -1,1 +1,11 @@
-print(Employee Module)
+print("Employee Details")
+
+print("Employee Search")
+
+print("Employee Update")
+
+print("Search By ID")
+
+print("Search By Name")
+
+print("Search By Department")
--- login.py
+++ login.py
@@ -1,1 +1,7 @@
-print(Login Module)
+print("Employee Login")
+
+print("Forgot Password")
+
+print("OTP Verification")
+
+print("Login Success")
